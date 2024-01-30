2f0d3d5894e77d80aa5bb191e60be2522de6303e
Kacper Majcher <majchermajus@proton.me>
2024-01-30 13:38:40 +0100

    fix: make path multi os support

--- src/utils/manage-cdn.ts
+++ src/utils/manage-cdn.ts
@@ -1,18 +1,21 @@
 import prisma from "@/utils/db";
 import fs from "fs";
+import path from "path";
+
+const root = path.join(path.parse(__dirname).dir.split(".next")[0], "public", "cdn")
 
 export const removeFile = async (id: string) => {
   try {
     const file = await prisma.galery.findFirst({ where: { id } });
     if (file) {
       await prisma.galery.delete({ where: { id: file.id } });
-      await fs.unlinkSync("./public/cdn/" + file.filename);
+      await fs.unlinkSync(path.join(root, file.filename));
     }
   } catch (error) {}
 };
 
 export const addFile = async (title: string, filename: string, data: any) => {
-  await fs.writeFileSync("./public/cdn/" + filename, data);
+  await fs.writeFileSync(path.join(root, filename), data);
   await prisma.galery.create({
     data: {
       title,
